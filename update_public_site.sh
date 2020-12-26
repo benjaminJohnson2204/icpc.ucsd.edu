d18cc8dca4837ef48e450664a77e50d98ddf3e7f
Max Morehead <moreheadmax@gmail.com>
2020-12-26 09:29:32 -0800

    Change update script

--- update_public_site.sh
+++ update_public_site.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-hugo && rsync -a public/ icpc@acsweb.ucsd.edu:public_html
+pushd "$(dirname "${BASH_SOURCE[0]}")"
+killall hugo
+hugo --cleanDestinationDir && rsync -av --delete public/ icpc@acsweb.ucsd.edu:public_html
+popd
